39c1c851563e6d85c2f45624054b7cc333338010
unknown <buragohain4@gmail.com>
2022-08-15 18:24:07 +0530

    Implemented UCs

---
+++ LinkedListProblems/LinkedListProblems/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace LinkedListProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LInkedList Problems: \n");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+            Console.WriteLine("\nAfter Deleting First Node");
+            list.RemoveFirstNode();
+            list.Display();
+            Console.WriteLine("\nAfter Deleting Last Node");
+            list.RemoveLastNode();
+            list.Display();
+            Console.WriteLine("\nNew LinkedList");
+            LinkedList list1 = new LinkedList();
+            list1.Add(56);
+            list1.Add(30);
+            list1.Add(70);
+            list1.Display();
+            Console.WriteLine("\nAfter Inserting Element");
+            list1.InserAtParticularPosition(3, 40);
+            list1.Display();
+            Console.WriteLine("\nPosition of 30--> " + list1.findPoistion(30));
+            list1.deleteNode(40);
+            Console.WriteLine("After Deleting Node");
+            list1.Display();
+            Console.WriteLine("Linkelist Size " + list1.count());
+            LinkedList list2 = new LinkedList();
+            list2.SortedList(56);
+            list2.SortedList(30);
+            list2.SortedList(70);
+            list2.SortedList(40);
+            Console.Write("Sorted List is :");
+            list2.Display();
+            Console.ReadLine();
+        }
+    }
+}
